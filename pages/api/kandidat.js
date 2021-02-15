02b5e71ecaeeca5f363411aa2700ef9e50fb27f5
Nur Fikri <codingki@gmail.com>
2021-02-15 22:23:51 +0700

    test cors

--- pages/api/kandidat.js
+++ pages/api/kandidat.js
@@ -1,4 +1,21 @@
 import { getKandidat } from './fetch';
+import Cors from 'cors';
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+	origin: '*',
+});
+
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
 
 export default async function handler(req, res) {
 	const allData = (await getKandidat()) || [];
@@ -6,5 +23,6 @@ export default async function handler(req, res) {
 		allKandidatVaksins: allData.allKandidatVaksins,
 		lastUpdate: allData.penelitianVaksin.lastUpdate,
 	};
+	await runMiddleware(req, res, cors);
 	res.status(200).json(data);
 }
