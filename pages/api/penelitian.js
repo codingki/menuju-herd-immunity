0804c39b8c2b01981512577046ab9d039ef11be8
Nur Fikri <codingki@gmail.com>
2021-02-15 22:28:35 +0700

    cors added

--- pages/api/penelitian.js
+++ pages/api/penelitian.js
@@ -1,6 +1,24 @@
 import { getPenilitian } from './fetch';
+import Cors from 'cors';
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+	origin: '*',
+});
+
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
 
 export default async function handler(req, res) {
 	const allData = (await getPenilitian()) || [];
+	await runMiddleware(req, res, cors);
 	res.status(200).json(allData);
 }
